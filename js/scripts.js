6d24d2881168c29a7fe347fab01f033c5669ec2e
Ashlin Aronin & Jordan Johansen <student@epicodus.com>
2015-09-08 10:41:51 -0700

    Finish leap year stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0)) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
         return true;
     } else {
         return false;
@@ -12,8 +12,11 @@ $(document).ready(function() {
         var result = leapYear(year);
 
         $(".year").text(year);
+
         if (!result) {
             $(".not").text("not");
+        } else {
+            $(".not").empty();
         }
 
         $("#result").show();
